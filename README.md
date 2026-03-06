2545af96c806880f8d182298cbd8affaf2cfa09a
Helen Qian <helenqian@MacBook-Pro-35.local>
2026-03-05 20:48:37 -0500

    partioner, lstm model

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
 # FedAvg Algorithm: Shakespeare LSTM example
+
 the goal of this project is to implement the Shakepseare dataset example from the FedAvg paper
 
-note: initialized as a flower pytorch example 
+note: initialized as a flower pytorch example
 tutorial to learn: https://github.com/adap/flower/blob/main/examples/flower-in-30-minutes/tutorial.ipynb
 
 ## notes
+
 make sure to setup virtual environment
 `python3 -m venv .venv`
 and then install necessary packages (within quickstart-python dir)
@@ -12,9 +14,10 @@ and then install necessary packages (within quickstart-python dir)
 (takes it from the pyproject.toml)
 
 ## implementation steps
+
 - [x] clean data
-- [ ] create partitoner to simulate federation: partition clients by 'Player' identifier
-- [ ] make LSTM model architecture ( Net() inside task.py)
+- [x] create partitoner to simulate federation: partition clients by 'Player' identifier
+- [x] make LSTM model architecture ( Net() inside task.py)
 - [ ] implement server code (load global model, set strategy to FedAvg, define global evaluation fn)
 - [ ] client.py (train function, local evaluation fn)
 - [ ] run with different hyperparameters and compare
--- pytorchexample/task.py
+++ pytorchexample/task.py
@@ -11,29 +11,41 @@ import matplotlib.pyplot as plt
 
 
 class Net(nn.Module):
-    """Model (simple CNN adapted from 'PyTorch: A 60 Minute Blitz')"""
 
-    def __init__(self):
+    def __init__(self, vocab_size: int = VOCAB_SIZE,
+                 embed_dim: int = 8,
+                 hidden_dim: int = 256,
+                 num_layers: int = 2):
         super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(3, 6, 5)
-        self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(16 * 5 * 5, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 10)
+
+        #Embedding
+        #maps each character index → dense vector of size 8.
+        self.embedding = nn.Embedding(vocab_size, embed_dim)
+
+        #LSTM
+        self.lstm == nn.LSTM(
+            input_size = embed_dim,
+            hidden_size = hidden_dim,
+            num_layers = num_layers,
+            batch_first = True,
+        )
+
+        #Linear classifierß
+        self.fc = nn.Linear(hidden_dim, vocab_size) #num_classes number of unique players
 
     def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = x.view(-1, 16 * 5 * 5)
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        return self.fc3(x)
+        # x shape: (batch_size, seq_len) 
 
+        embedded = self.embedding(x)
+
+        lstm_out, _ = self.lstm(embedded)
+
+        logits = self.fc(lstm_out)
+        return logits
 
 partitioner = None  # Cache partitioner
 
-pytorch_transforms = Compose([ToTensor(), Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+pytorch_transforms = Compose([ToTensor(), Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]) #converts PIL images to tensors (text data)
 
 
 def apply_transforms(batch):
@@ -48,7 +60,7 @@ def load_data(partition_id: int, num_partitions: int, batch_size: int):
     global partitioner
     if partitioner is None:
         df = pd.read_csv('./data/Shakespeare_cleaned.csv')
-        hf_dataset = Dataset.from_pandas(df)
+        hf_dataset = Dataset.from_pandas(df) #load into csv into pandas dataframe
         # make partitioner 
         partitioner = NaturalIdPartitioner(partition_by="Player")
         partitioner.dataset = hf_dataset
